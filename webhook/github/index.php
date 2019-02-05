29f53c2f974ed8c1ff39b3f8ad90c367c0d07e1a
Chris Zuber <shgysk8zer0@gmail.com>
2019-02-05 00:33:43 -0800

    Update to GitHub Webhook

--- webhook/github/index.php
+++ webhook/github/index.php
@@ -2,50 +2,42 @@
 namespace WebHook\GitHub;
 
 use const \Consts\{GITHUB_WEBHOOK};
-use \shgysk8zer0\{HTTPException, Headers};
+use \shgysk8zer0\{HTTPException, Headers, API};
 use \shgysk8zer0\WebHook\{GitHub};
 use \Throwable;
 
-const METHODS = 'POST, OPTIONS, HEAD';
+const METHODS = ['POST', 'OPTIONS','HEAD'];
+
+function info(): void
+{
+	Headers::set('Allow', join(', ', METHODS));
+}
 
 require_once(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'autoloader.php');
 
-try {
-	switch($_SERVER['REQUEST_METHOD']) {
-	case 'POST':
-		$hook = new GitHub(GITHUB_WEBHOOK);
-		switch ($hook->event) {
-			case 'ping':
-				Headers::set('Content-Type', 'application/json');
-				echo json_encode($hook);
-				break;
-			case 'push':
-				Headers::set('Content-Type', 'text/plain');
-				if ($hook->isMaster()) {
-					echo `git pull`;
-					echo `git submodule update --init --recursive`;
-					echo `git status`;
-				} else {
-					echo 'Not updating non-master branch';
-				}
-				break;
-			default:
-				throw new HTTPException(sprintf('Unsupported event: %s', $hook->event), HTTP::NOT_IMPLEMENTED);
-		}
-	case 'OPTIONS':
-	case 'HEAD':
-		Headers::set('Allow', METHODS);
-		break;
-	default:
-		Headers::set('Allow', METHODS);
-		throw new HTTPException('Allowed Methods: ' . METHODS, Headers::METHOD_NOT_ALLOWED);
+$api = new API('*', METHODS);
+$api->on('POST', function(API $api): void
+{
+	$hook = new GitHub(GITHUB_WEBHOOK);
+	switch ($hook->event) {
+		case 'ping':
+			Headers::set('Content-Type', 'application/json');
+			echo json_encode($hook);
+			break;
+		case 'push':
+			Headers::set('Content-Type', 'text/plain');
+			if ($hook->isMaster()) {
+				echo `git pull`;
+				echo `git submodule update --init --recursive`;
+				echo `git status`;
+			} else {
+				echo 'Not updating non-master branch';
+			}
+			break;
+		default:
+			throw new HTTPException("Unsupported event: {$hook->event}", HTTP::NOT_IMPLEMENTED);
 	}
-} catch(HTTPException $e) {
-	Headers::status($e->getCode());
-	Headers::set('Content-Type', 'application/json');
-	echo json_encode($e);
-} catch (Throwable $e) {
-	Headers::status(Headers::INTERNAL_SERVER_ERROR);
-	Headers::set('Content-Type', 'text/plain');
-	echo 'Internal Server Error' . PHP_EOL;
-}
+});
+$api->on('OPTIONS', __NAMESPACE__ . '\info');
+$api->on('HEAD', __NAMESPACE__ . '\info');
+$api();
