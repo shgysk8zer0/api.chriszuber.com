327b380a3277eb979eceb682ac8ba5f8acd09670
Chris Zuber <shgysk8zer0@gmail.com>
2019-09-11 11:21:03 -0700

    Revert "Update Event searching"

--- consts.php
+++ consts.php
@@ -29,11 +29,6 @@ const CSP_ALLOWED_HEADERS = [
 	'Upgrade-Insecure-Requests',
 ];
 
-const TOKEN_EXPIRES = [
-	'value' => 1,
-	'units' => 'month',
-];
-
 define(__NAMESPACE__ . '\HOST', sprintf('%s://%s',
 	(array_key_exists('HTTPS', $_SERVER) and ! empty($_SERVER['HTTPS'])) ? 'https' : 'http',
 	$_SERVER['HTTP_HOST'] ?? 'localhost'
