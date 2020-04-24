a4463828c28e4789adc75a165630fe071ff17c58
Chris Zuber <shgysk8zer0@gmail.com>
2020-04-23 19:44:04 -0700

    Update linting

--- lint.php
+++ lint.php
@@ -1,22 +1,54 @@
 <?php
 namespace Lint;
 
-use \shgysk8zer0\PHPAPI\{Linter, Headers};
+use \shgysk8zer0\PHPAPI\{Linter, Headers, SAPILogger};
 use \shgysk8zer0\PHPAPI\Abstracts\{HTTPSTatusCodes as HTTP};
-use function \Functions\{is_cli};
+use \RuntimeException;
 
-require_once(__DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php');
+const CONFIG = './config.ini';
 
-if (is_cli()) {
-	$linter = new Linter();
-	$linter->ignoreDirs('./.git', './data', './logs');
-	$linter->scanExts('php');
+if (! PHP_SAPI === 'cli') {
+	Headers::status(HTTP::FORBIDDEN);
+} elseif (! isset($argv) or realpath($argv[0]) !== __FILE__) {
+	throw new RuntimeException('Linting must be executed directly');
+} elseif (! file_exists(CONFIG)) {
+	throw new RuntimeException('Missing linting config file');
+} elseif (! $config = parse_ini_file(CONFIG, true, INI_SCANNER_TYPED)) {
+	throw new RuntimeException('Error parsing linter config file');
+} else {
+	$args = getopt('d:', ['dir::']);
 
-	if ($linter->scan(__DIR__)) {
+	if (array_key_exists('dir', $args)) {
+		$dir = $args['dir'];
+	} elseif (array_key_exists('d', $args)) {
+		$dir = $args['d'];
+	} else {
+		$dir = __DIR__;
+	}
+
+	unset($args);
+
+	date_default_timezone_set($config['init']['timezone']);
+
+	foreach ($config['init']['require'] as $path) {
+		require_once $path;
+	}
+
+	spl_autoload_register($config['autoload']['function']);
+	spl_autoload_extensions(join(',', $config['autoload']['extensions']));
+	set_include_path(
+		join(PATH_SEPARATOR, $config['autoload']['path'])
+		. PATH_SEPARATOR . get_include_path()
+	);
+
+
+	$linter = new Linter(new SAPILogger());
+	$linter->ignoreDirs(...$config['lint']['ignore']);
+	$linter->scanExts(...$config['lint']['extensions']);
+
+	if ($linter->scan($dir)) {
 		exit(0);
 	} else {
 		exit(1);
 	}
-} else {
-	Headers::status(HTTP::FORBIDDEN);
 }
